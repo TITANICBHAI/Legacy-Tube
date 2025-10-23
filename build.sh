f76818036b634602bc8cfad3a4ffb221e62b19eb
TITANIC BHAI <himanshusmartwatch@gmail.com>
2025-10-23 13:59:49 +0530

    Make binaries executable and update PATH in build.sh
    Updated build script to make binaries executable and set PATH.

--- build.sh
+++ build.sh
@@ -5,8 +5,12 @@ set -o errexit
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
-echo "Making bundled ffmpeg/ffprobe executable..."
+echo "Making binaries executable..."
 chmod +x bin/ffmpeg bin/ffprobe
+
+# Add local bin to PATH for build and runtime
+export PATH="$PWD/bin:$PATH"
+echo "PATH set to: $PATH"
 
 echo "Creating download folder..."
 mkdir -p /tmp/downloads
